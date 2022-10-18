8aaf3af8e2f9317d49d1cf62136193e71a80bb24
Supreeta Vijayakumar <supreetabv@yahoo.co.uk>
2022-10-18 18:27:37 +0100

    Add files via upload

--- run_optimization.m
+++ run_optimization.m
@@ -1,10 +1,18 @@
-Ei=importdata('Einput_rice.txt'); % Load enzyme activity levels optimized at 280 ppm for rice
+Eidata=importdata('Einput_rice.txt'); % Load enzyme activity levels optimized at 280 ppm for rice
+Ei=Eidata.data;
 Einput=ones(37,1); % 
-CO2i = 200; % Set Ci values ranging from 200 - 360
+CO2i = 360; % Set Ci
 PPFDi = 2000; % Set light intensity
-WeatherTemp = 25; Set temperature
-global Vrubusco_adj; reset enzyme activity
+WeatherTemp = 25; % Set temperature
+global Vrubusco_adj; %reset enzyme activity
 Vrubusco_adj = 1.0;
 global VmaxAdj;% reset enzyme activity
 VmaxAdj = 1.0;
-NetAssimilation =  EPS_Drive_GRNs(Einput,CO2i,PPFDi,WeatherTemp,0,0,Ei); % Calculate net assimilation rate A for a given Ci value using set of enzyme activity levels optimized for 280
+global pcfactor;
+pcfactor=1;
+%Loop function
+%NetAssimilationRate = zeros(9,1);
+% for i=1:9
+% CO2i = (200:20:360)'; % Set Ci values ranging from 200 - 360
+A9 =  EPS_Drive_GRNs(Einput,CO2i,PPFDi,WeatherTemp,0,0,Ei); % Calculate net assimilation rate A for a given Ci value using set of enzyme activity levels optimized for 280
+%end
